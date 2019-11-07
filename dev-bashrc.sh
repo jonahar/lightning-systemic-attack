76ed5eb078ef8645c42079ecc361f7f38d0a40ae
Jona Harris <jonahar@gmail.com>
2019-11-07 09:34:54 +0200

    convert lncli aliases to functions

--- dev-bashrc.sh
+++ dev-bashrc.sh
@@ -2,7 +2,3 @@
 
 source $HOME/.bashrc
 PATH=$PATH:$LNPATH:$LNPATH/btcd:$LNPATH/lnd
-
-alias lncli-alice="cd $LNPATH/dev/alice; lncli --rpcserver=localhost:10001 --macaroonpath=data/chain/bitcoin/simnet/admin.macaroon"
-alias lncli-bob="cd $LNPATH/dev/bob; lncli --rpcserver=localhost:10002 --macaroonpath=data/chain/bitcoin/simnet/admin.macaroon"
-alias lncli-charlie="cd $LNPATH/dev/charlie; lncli --rpcserver=localhost:10003 --macaroonpath=data/chain/bitcoin/simnet/admin.macaroon"
--- setup-env.sh
+++ setup-env.sh
@@ -2,20 +2,48 @@
 
 export LNPATH="/cs/usr/jonahar/ln"
 
+
+lncli-alice(){
+    builtin cd $LNPATH/dev/alice;
+    lncli --rpcserver=localhost:10001 --macaroonpath=data/chain/bitcoin/simnet/admin.macaroon $*
+    builtin cd - # return to previous directory
+}
+
+lncli-bob(){
+    builtin cd $LNPATH/dev/bob;
+    lncli --rpcserver=localhost:10002 --macaroonpath=data/chain/bitcoin/simnet/admin.macaroon $*
+    builtin cd -
+}
+
+lncli-charlie(){
+    builtin cd $LNPATH/dev/charlie;
+    lncli --rpcserver=localhost:10003 --macaroonpath=data/chain/bitcoin/simnet/admin.macaroon $*
+    builtin cd -
+}
+
+
+# export the custom lnclis to subshells
+export -f lncli-alice
+export -f lncli-bob
+export -f lncli-charlie
+
+
 # commands to run in the different tmux windows
 BTCD_COMMAND="cd ${LNPATH}; run-btcd.sh"
-
 ALICE_COMMAND="cd ${LNPATH}/dev/alice; bash --init-file ${LNPATH}/dev-bashrc.sh"
 BOB_COMMAND="cd ${LNPATH}/dev/bob; bash --init-file ${LNPATH}/dev-bashrc.sh"
 CHARLIE_COMMAND="cd ${LNPATH}/dev/charlie; bash --init-file ${LNPATH}/dev-bashrc.sh"
+LNCLI_COMMAND="bash --init-file ${LNPATH}/dev-bashrc.sh"
+
+
+# kill any existing tmux sessions
+tmux kill-server; sleep 1;
 
 tmux new-session -d -n btcd "$BTCD_COMMAND"
 tmux new-window -n a-lnd "$ALICE_COMMAND"
 tmux new-window -n b-lnd "$BOB_COMMAND"
 tmux new-window -n b-lnd "$CHARLIE_COMMAND"
 
-tmux new-window -n a-cli "$ALICE_COMMAND"
-tmux new-window -n b-cli "$BOB_COMMAND"
-tmux new-window -n c-cli "$CHARLIE_COMMAND"
+tmux new-window -n lncli "$LNCLI_COMMAND"
 
 tmux attach-session
