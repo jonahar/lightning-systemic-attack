7b9f7a772b3a0a3f39e521b3919c66cd9b040176
Jona Harris <jonahar@gmail.com>
2019-11-26 11:38:50 +0200

    modify commands in run.py

--- py/run.py
+++ py/run.py
@@ -1,23 +1,11 @@
 import os
-import time
 
-from lightning import LightningRpc  # pip3 install pylightning
+from lightning import LightningRpc, Millisatoshi  # pip3 install pylightning
 
-from bitcoin_cli import (
-    get_transaction,
-    mine,
-)
+from bitcoin_cli import get_mempool_txids, get_transaction, mine
 from lightning_cli import (
     get_id,
-    get_total_balance,
     make_many_payments,
-)
-from tx_set import TXSet
-from utils import (
-    find_interesting_txids,
-    print_json,
-    show_num_tx_in_last_t_blocks,
-    show_tx_in_block,
 )
 
 lnpath = os.path.expandvars("$LNPATH")
@@ -30,52 +18,21 @@ n3 = LightningRpc(os.path.join(lnpath, "lightning-dirs/3/lightning-rpc"))
 ab_funding_txid = n1.listpeers(peerid=get_id(n2))["peers"][0]["channels"][0]["funding_txid"]
 bc_funding_txid = n2.listpeers(peerid=get_id(n3))["peers"][0]["channels"][0]["funding_txid"]
 
-# send 0.02 to Bob
-make_many_payments(
-    sender=n1,
-    receiver=n2,
-    num_payments=1,
-    msatoshi_per_payment=2_000_000_000,
-)
+amount = Millisatoshi("0.0001btc")
 
-# send 0.01 to Charlie, which would result in an unresolved HTLC (assuming charlie is evil)
+# send many payments to Charlie, which would result in unresolved HTLCs (assuming charlie is evil)
 make_many_payments(
     sender=n1,
     receiver=n3,
-    num_payments=1,
-    msatoshi_per_payment=1_000_000_000,
+    num_payments=480,
+    msatoshi_per_payment=amount.millisatoshis,
 )
 
 # force close so we can see the commitment transaction
-ab_commitment = n1.close(peer_id=get_id(n2), force=True, timeout=0)["txid"]
+bc_commitment = n3.close(peer_id=get_id(n2), force=True, timeout=0)["txid"]
+mine(1)
 
-# ----------------
-
-# # shutdown node 2
-# n2.stop()
-#
-# # force close the channel
-# bob_charlie_closing_txid = n3.close(peer_id=get_id(n2), force=True, timeout=0)['txid']
-# mine(1)
-#
-# for i in range(20):
-#     mine(1)
-#     time.sleep(1)
-#
-# current_height = n1.getinfo()['blockheight']
-# txids = find_interesting_txids(block_heights=range(current_height - 30, current_height + 1))
-# tx_set = TXSet(txids=txids)
-#
-# # see the funding transaction
-# print_json(get_transaction(bc_funding_txid))
-#
-# # see the closing transaction
-# print_json(get_transaction(bob_charlie_closing_txid))
-#
-# show_num_tx_in_last_t_blocks(t=3)
-#
-# show_tx_in_block(414)
-#
-# get_total_balance(n1)
-# get_total_balance(n2)
-# get_total_balance(n3)
+# the mempool, after bob sees the commitment tx onchain
+mempool = get_mempool_txids()
+total_size = sum(map(lambda txid: get_transaction(txid)["size"], mempool))
+total_vsize = sum(map(lambda txid: get_transaction(txid)["vsize"], mempool))
