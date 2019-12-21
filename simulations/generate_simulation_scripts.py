bad8feb2bdf806b0a24d1be588c39b6d5bf4d51d
Jona Harris <jonahar@gmail.com>
2019-12-21 11:04:04 +0200

    use the LN variable instead of hardcoded path in generate_simulation_scripts.py

--- simulations/generate_simulation_scripts.py
+++ simulations/generate_simulation_scripts.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
-topology = "/home/jona/lab/ln/topologies/topology-1-10-1.json"
+import os
+
+ln = os.path.expandvars("$LN")
+simulations_dir = os.path.join(ln, "simulations")
+
+topology = os.path.join(ln, "topologies/topology-1-10-1.json")
 num_victims = 10
 num_payments = num_victims * 500
 amount_msat = int((0.1 / 500) * (10 ** 8) * (10 ** 3))
@@ -10,8 +15,8 @@ blockmaxweight_values = [100000, 250000, 500000, 750000, 1000000]
 
 for blockmaxweight in blockmaxweight_values:
     script_name = f"steal-attack-{num_victims}victims-blockmaxweight={blockmaxweight}"
-    datadir = f"/home/jona/lab/ln/simulations/data-{script_name}"
-    script_file = f"/home/jona/lab/ln/simulations/{script_name}.sh"
+    datadir = os.path.join(simulations_dir, f"data-{script_name}")
+    script_file = os.path.join(simulations_dir, f"{script_name}.sh")
     
     with open(script_file, mode="w") as f:
         simulation_script = f"""#!/usr/bin/env bash
