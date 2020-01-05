5d919b2e12c13941d45cae9fe82ed0779289f55b
Jona Harris <jonahar@gmail.com>
2020-01-05 13:08:47 +0200

    estimatesmartfee: rename estimations file; set smarter axes ticks

--- py/parse_estimatesmartfee_samples.py
+++ py/parse_estimatesmartfee_samples.py
@@ -1,14 +1,22 @@
 import os
 import re
+from typing import List, Tuple
 
 import matplotlib.pyplot as plt
+import numpy as np
+
+TIMESTAMPS = List[float]
+FEERATES = List[float]
+LABEL = str
 
 ln = os.path.expandvars("$LN")
 samples_dir = os.path.join(ln, "estimatesmartfee-samples")
+estimations_files = os.listdir(samples_dir)
 
-samples_file_regex = re.compile("estimates-blocks=(\\d+),mode=(\\w+)")
+samples_file_regex = re.compile("fee-estimations_blocks=(\\d+)_mode=(\\w+)")
 
-for entry in os.listdir(samples_dir):
+data: List[Tuple[TIMESTAMPS, FEERATES, LABEL]] = []
+for entry in estimations_files:
     match = samples_file_regex.match(entry)
     if not match:
         print(f"Ignoring file in unrecognized format: {entry}")
@@ -17,16 +25,27 @@ for entry in os.listdir(samples_dir):
     mode: str = match.group(2)
     with open(os.path.join(samples_dir, entry)) as f:
         lines = f.read().splitlines()
-    timestamps = list(map(lambda line: line.split(":")[0], lines))
-    feerates = list(map(lambda line: line.split(":")[1], lines))
-    plt.plot(
-        timestamps, feerates,
-        label=f"estimatesmartfee(block={blocks},mode={mode})",
+    timestamps = list(map(lambda line: int(line.split(":")[0]), lines))
+    feerates = list(map(lambda line: float(line.split(":")[1]), lines))
+    data.append(
+        (timestamps, feerates, f"estimatesmartfee(block={blocks},mode={mode})")
     )
+
+for timestamps, feerates, label in data:
+    plt.plot(timestamps, feerates, label=label)
+
+min_timestamp = min(min(t) for t, f, l in data)
+max_timestamp = max(max(t) for t, f, l in data)
+min_feerate = min(min(f) for t, f, l in data)
+max_feerate = max(max(f) for t, f, l in data)
 
 # graph config
 plt.legend(loc='best')
+plt.title('estimated feerate')
+
 plt.xlabel('timestamp')
+plt.xticks(np.linspace(start=min_timestamp, stop=max_timestamp, num=10))
+
 plt.ylabel('feerate')
-plt.title('estimated vs actual required feerate')
+plt.yticks(np.linspace(start=min_feerate, stop=max_feerate, num=10))
 plt.show()
