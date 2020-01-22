7008d4d328ab7aaadca9e438e98d484fb0f1efb0
Jona Harris <jonahar@gmail.com>
2020-01-22 18:48:44 +0200

    utils: add profiling wrapper

--- py/utils.py
+++ py/utils.py
@@ -1,5 +1,7 @@
 import json
+import time
 from datetime import datetime
+from functools import wraps
 
 from datatypes import Json
 
@@ -13,3 +15,20 @@ def now() -> str:
     return current time in YYYY-MM-DD_HH:MM
     """
     return datetime.now().strftime("%Y-%m-%d_%H:%M")
+
+
+def timeit(func):
+    # the `wraps` decorator gives `wrapper` the attributes of func.
+    # in particular, its name
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print(f"Entering {func.__name__}")
+        t0 = time.time()
+        result = func(*args, **kwargs)
+        t1 = time.time()
+        print(
+            f"Exiting {func.__name__}. function ran for {t1 - t0} seconds"
+        )
+        return result
+    
+    return wrapper
