ad0d48549d2723f2143d93b5bfbfc36918701787
Jona Harris <jonahar@gmail.com>
2020-01-22 16:33:53 +0200

    utils: add now() method

--- py/utils.py
+++ py/utils.py
@@ -1,7 +1,15 @@
 import json
+from datetime import datetime
 
 from datatypes import Json
 
 
 def print_json(o: Json):
     print(json.dumps(o, indent=4))
+
+
+def now() -> str:
+    """
+    return current time in YYYY-MM-DD_HH:MM
+    """
+    return datetime.now().strftime("%Y-%m-%d_%H:%M")
