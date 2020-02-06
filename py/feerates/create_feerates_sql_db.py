e633ff9a3e9dbc9be57722e7d5170c4992b9ed14
Jona Harris <jonahar@gmail.com>
2020-02-06 11:43:30 +0200

    move parallelism across txs in single block instead of across multiple blocks

--- py/feerates/create_feerates_sql_db.py
+++ py/feerates/create_feerates_sql_db.py
@@ -12,6 +12,37 @@ from feerates.tx_fee_oracle import TXFeeOracle
 MAX_WORKERS = 16
 
 
+def __dump_block_feerates_to_file(h: BlockHeight, oracle: TXFeeOracle, filepath: str) -> bool:
+    """
+    helper for dump_block_feerates. write results to a file in the given path.
+    returns true if all feerates were successfully written to the file
+    """
+    block: Block = get_block_by_height(h)
+    executor = ThreadPoolExecutor(max_workers=MAX_WORKERS)
+    # submit all jobs to the pool
+    txid_to_future = {
+        txid: executor.submit(TXFeeOracle.get_tx_feerate, oracle, txid)
+        for txid in block["tx"]
+    }
+    
+    with open(filepath, mode="w") as f:
+        # iterate all futures, extract the computed result and dump to the file
+        for txid, future in txid_to_future.items():
+            feerate = future.result()
+            if feerate is None:
+                # we give up on the entire block if we fail to get the feerate of at least one transaction
+                # cancel all remaining jobs
+                for f in txid_to_future.values():
+                    f.cancel()
+                executor.shutdown(wait=False)
+                return False
+            
+            f.write(f"{txid},{feerate}\n")
+    
+    executor.shutdown(wait=True)
+    return True
+
+
 def dump_block_feerates(h: BlockHeight, oracle: TXFeeOracle) -> None:
     """
     computes the feerate of all transactions in block at height h and dump them
@@ -24,30 +55,16 @@ def dump_block_feerates(h: BlockHeight, oracle: TXFeeOracle) -> None:
     
     # use tmp suffix until we finish with that block (in case we crash before we dumped all txs)
     filepath_tmp = f"{filepath}.tmp"
-    with open(filepath_tmp, mode="w") as f:
-        block: Block = get_block_by_height(h)
-        # we dump all transactions, including coinbase
-        for txid in block["tx"]:
-            feerate = oracle.get_tx_feerate(txid)
-            if feerate is None:
-                logger.error(f"Oracle couldn't retrieve feerate for a transaction in block {h}")
-                return
-            f.write(f"{txid},{feerate}\n")
-    
-    os.rename(filepath_tmp, filepath)
+    success = __dump_block_feerates_to_file(h=h, oracle=oracle, filepath=filepath_tmp)
+    if success:
+        os.rename(filepath_tmp, filepath)
+    else:
+        logger.error(f"Oracle couldn't retrieve feerate for a transaction in block {h}")
 
 
-def dump_block_feerates_serial(first_block: int, last_block: int, oracle: TXFeeOracle) -> None:
+def dump_blocks_feerates(first_block: int, last_block: int, oracle: TXFeeOracle) -> None:
     for h in range(first_block, last_block + 1):
         dump_block_feerates(h=h, oracle=oracle)
-
-
-def dump_block_feerates_parallel(first_block: int, last_block: int, oracle: TXFeeOracle) -> None:
-    with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
-        futures = [
-            executor.submit(fn=dump_block_feerates, h=h, oracle=oracle)
-            for h in range(first_block, last_block + 1)
-        ]
 
 
 if __name__ == "__main__":
@@ -59,7 +76,7 @@ if __name__ == "__main__":
     
     oracle = BitcoindTXFeeOracle(next_oracle=None)
     
-    dump_block_feerates_parallel(first_block=first_block, last_block=last_block, oracle=oracle)
+    dump_blocks_feerates(first_block=first_block, last_block=last_block, oracle=oracle)
 
 """
 # After we dumped all blocks feerates, we wish to put them all in an SQLite DB:
