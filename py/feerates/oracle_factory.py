70561a499b513b6638f1d391f463e38ce1ce5af5
Jona Harris <jonahar@gmail.com>
2020-02-09 19:50:55 +0200

    use leveldb as default oracle. it is both smaller in size and faster than sqlite

--- py/feerates/oracle_factory.py
+++ py/feerates/oracle_factory.py
@@ -1,7 +1,7 @@
 import os
 
 from feerates.bitcoind_oracle import BitcoindTXFeeOracle
-from feerates.sql_oracle import SQLTXFeeOracle
+from feerates.level_db_oracle import LevelDBTXFeeOracle
 from feerates.tx_fee_oracle import TXFeeOracle
 
 ln = os.path.expandvars("$LN")
@@ -17,8 +17,8 @@ index_dir = "/cs/labs/avivz/jonahar/bitcoin-datadir/blocks/index"
 def get_multi_layer_oracle() -> TXFeeOracle:
     layer_1 = BitcoindTXFeeOracle(next_oracle=None)
     
-    layer_0 = SQLTXFeeOracle(
-        db_filepath=SQLITE_DB_FILEPATH,
+    layer_0 = LevelDBTXFeeOracle(
+        db_filepath=LEVELDB_FILEPATH,
         next_oracle=layer_1,
     )
     return layer_0
