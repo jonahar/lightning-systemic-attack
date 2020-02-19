0a6b712524ea6019cb4ba6db7e699cd69f427de0
Jona Harris <jonahar@gmail.com>
2020-02-19 14:53:20 +0200

    generate_simulation_scripts: change running values + set different output file, based on simulation number

--- py/generate_simulation_scripts.py
+++ py/generate_simulation_scripts.py
@@ -8,13 +8,13 @@ simulations_dir = os.path.join(ln, "simulations")
 # set these to customize the simulation
 topology = os.path.join(ln, "topologies/topology-1-5-1.json")
 num_victims = 5
-payments_per_victim = 400
+payments_per_victim = 483
 channel_balance = 0.1
 num_blocks = 200
 block_time = 150
-blockmaxweight_values = [100000]
-htlcs = [50, 100, 200, 300, 400]
-delays = [4, 5, 6, 7, 8, 9, 10]
+blockmaxweight_values = [100_000, 200_000, 300_000, 400_000]
+htlcs = [483]
+delays = [10]
 
 # the following are derived from the previous variables. probably shouldn't be modified
 amount_msat = int(
@@ -38,6 +38,8 @@ for blockmaxweight, htlc, delay in product(blockmaxweight_values, htlcs, delays)
     with open(script_file, mode="w") as f:
         simulation_script = f"""#!/usr/bin/env bash
         cd $LN/py
+        SIMULATION=1
+        COMMANDS_FILE=$LN/generated_commands_$SIMULATION
         python3 -m commands_generator.commands_generator \\
             --topology "{topology}" \\
             --establish-channels \\
@@ -46,11 +48,11 @@ for blockmaxweight, htlc, delay in product(blockmaxweight_values, htlcs, delays)
             --dump-data "{datadir}" \\
             --block-time {block_time} \\
             --bitcoin-blockmaxweight {blockmaxweight} \\
-            --simulation-number 1 \\
-            --outfile $LN/generated_commands
+            --simulation-number $SIMULATION \\
+            --outfile $COMMANDS_FILE
         
         
-        bash generated_commands 2>&1 | tee {output_file}
+        bash $COMMANDS_FILE 2>&1 | tee {output_file}
         exit 0
         """
         
