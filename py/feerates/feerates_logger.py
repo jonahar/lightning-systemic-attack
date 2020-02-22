13728907301c043653b548001b1bee81c78b52f9
Jona Harris <jonahar@gmail.com>
2020-02-22 17:59:42 +0200

    change to default feerates logger to use a rotating file handler (our feerates dump process runs forever and the logfile may grow without bounds)

--- py/feerates/feerates_logger.py
+++ py/feerates/feerates_logger.py
@@ -1,10 +1,30 @@
+import logging
 import os
+import sys
+from logging.handlers import RotatingFileHandler
 
-from utils import setup_logging
+from paths import LN
 
-ln = os.path.expandvars("$LN")
+logger = logging.getLogger("feerates_logger")
 
-logger = setup_logging(
-    logger_name="feerates_logger",
-    filename=os.path.join(ln, "feerates.log"),
+fmt = "%(asctime)s: %(levelname)s: %(module)s: %(funcName)s: %(message)s"
+formatter = logging.Formatter(fmt=fmt, datefmt='%Y-%m-%d %H:%M:%S')
+logger.setLevel(logging.DEBUG)
+
+# file handler. we use RotatingFileHandler since this logger may be used
+# indefinitely (e.g. for the feerates dump process)
+logfile = os.path.join(LN, "feerates.log")
+file_handler = RotatingFileHandler(
+    filename=logfile,
+    maxBytes=50 * 1024 * 1024,  # 50 MB
+    backupCount=1,
 )
+file_handler.setFormatter(formatter)
+file_handler.setLevel("DEBUG")
+logger.addHandler(file_handler)
+
+# console handler
+console_handler = logging.StreamHandler(stream=sys.stdout)
+console_handler.setFormatter(formatter)
+console_handler.setLevel("DEBUG")
+logger.addHandler(console_handler)
