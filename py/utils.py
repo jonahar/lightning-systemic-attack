8099e3574aaefd1fe66dcc6a240431f1ae3ad226
Jona Harris <jonahar@gmail.com>
2020-02-24 11:12:07 +0200

    utils: implement populate_leveldb_cache to speedup caches' first-time population

--- py/utils.py
+++ py/utils.py
@@ -7,7 +7,7 @@ import time
 from datetime import datetime
 from functools import wraps
 from logging import Logger
-from typing import Any
+from typing import Any, Callable
 
 import plyvel
 
@@ -96,6 +96,24 @@ def setup_logging(
 CACHES_DIR = os.path.join(os.path.expandvars("$LN"), "data", "caches")
 
 
+def get_db_key(*args, **kwargs) -> bytes:
+    return pickle.dumps(
+        (args, sorted(kwargs.items()))
+    )
+
+
+def serialize_value(value: Any) -> bytes:
+    return pickle.dumps(value)
+
+
+def deserialize_value(bytes) -> Any:
+    return pickle.loads(bytes)
+
+
+def get_leveldb_cache_fullpath(func_name: str) -> str:
+    return os.path.join(CACHES_DIR, f"{func_name}_py_function_leveldb")
+
+
 def leveldb_cache(func):
     """
     this is a decorator that caches result for the function 'func'.
@@ -103,28 +121,17 @@ def leveldb_cache(func):
     
     The cache size is (currently) not configurable, and is unlimited
     """
-    cache_name = f"{func.__name__}_py_function_leveldb"
+    
     try:
-        cache_fullpath = os.path.join(CACHES_DIR, cache_name)
+        cache_fullpath = get_leveldb_cache_fullpath(func_name=func.__name__)
         db = plyvel.DB(cache_fullpath, create_if_missing=True)
     except plyvel.IOError:
         print(
-            f"WARNING: leveldb_cache: IOERROR occurred when trying to open leveldb `{cache_name}`. "
-            f"function `{func.__name__}` will NOT be cached",
+            f"WARNING: leveldb_cache: IOERROR occurred when trying to open leveldb "
+            f"for function `{func.__name__}`. function will NOT be cached",
             file=sys.stderr,
         )
         return func
-    
-    def get_db_key(*args, **kwargs) -> bytes:
-        return pickle.dumps(
-            (args, sorted(kwargs.items()))
-        )
-    
-    def serialize_value(value: Any) -> bytes:
-        return pickle.dumps(value)
-    
-    def deserialize_value(bytes) -> Any:
-        return pickle.loads(bytes)
     
     @wraps(func)
     def cached_func(*args, **kwargs):
@@ -139,3 +146,53 @@ def leveldb_cache(func):
         return value
     
     return cached_func
+
+
+TSV_SEPARATOR = "\t"
+
+
+def populate_leveldb_cache(
+    tsv_file: str,
+    func_name: str,
+    str_key_to_py_object: Callable[[str], Any] = None,
+    str_value_to_py_object: Callable[[str], Any] = None,
+) -> None:
+    """
+    This is a helper method for populating caches to be used by the leveldb_cache decorator.
+    
+    It is useful in cases we want to cache a function but we want to populate it
+    manually because the first population is too expensive to be done by the cached function.
+    
+    This helper takes a tsv file with exactly two columns. the first value represents
+    a function argument and the second value represents a function result.
+    
+    The cached function is assumed to take exactly one argument and that it is called
+    like FUNC_NAME(arg1). i.e. the single argument is given without keyword
+    
+    Naturally, the argument and result are read from the tsv file as strings.
+    if str_key_to_py_object/str_value_to_py_object are provided, they should
+    be callables that take a string representation of the key/value respectively
+    and return a python object.
+    
+    
+    For example, if we'd like to populate a cache for the following function:
+    
+    def get_tx_size(txid:str) -> int:
+        ...
+    
+    we should make the following call:
+        populate_leveldb_cache("path/to/tsv", "get_tx_size", str_value_to_py_object=int)
+    
+    """
+    db = plyvel.DB(get_leveldb_cache_fullpath(func_name=func_name), create_if_missing=True)
+    with open(tsv_file) as f:
+        for i, line in enumerate(f):
+            line = line.strip()  # remove newline if exist
+            k, v = line.split(TSV_SEPARATOR)
+            if str_key_to_py_object:
+                k = str_key_to_py_object(k)
+            if str_value_to_py_object:
+                v = str_value_to_py_object(v)
+            
+            # k is the argument and v is the result
+            db.put(get_db_key(k), serialize_value(v))
