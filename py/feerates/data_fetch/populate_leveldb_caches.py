5751a2f0f814078a2dbd807fbf84712b4997cb73
Jona Harris <jonahar@gmail.com>
2020-02-28 19:47:55 +0200

    populate_leveldb_caches: claim the future results so exceptions will be raised (if any)

--- py/feerates/data_fetch/populate_leveldb_caches.py
+++ py/feerates/data_fetch/populate_leveldb_caches.py
@@ -10,6 +10,11 @@ MAX_WORKERS = None  # will be set by the executor according to number of CPUs
 
 
 def populate_block(h: BlockHeight) -> None:
+    """
+    populate the tx feerates and tx weights caches with txs in block h.
+    if this method returns, population is done successfully. if a problem occurred
+    an exception will be raised
+    """
     # all we need to do to populate the db with tx weights in the given block is to
     # query get_tx_weight for these weights
     
@@ -17,9 +22,11 @@ def populate_block(h: BlockHeight) -> None:
     txids = get_txs_in_block(height=h)
     
     with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
-        for txid in txids:
-            executor.submit(get_tx_feerate, txid)
-            executor.submit(get_tx_weight, txid)
+        futures = set()
+        futures.update(executor.submit(get_tx_feerate, txid) for txid in txids)
+        futures.update(executor.submit(get_tx_weight, txid) for txid in txids)
+        for future in futures:
+            future.result()  # this will raise if get_tx_feerate/get_tx_weight raised an exception
 
 
 def populate_blocks(first_block: BlockHeight, last_block: BlockHeight) -> None:
@@ -54,7 +61,7 @@ def parse_args():
     return parser.parse_args()
 
 
-if __name__ == "__main__":
+def main():
     args = parse_args()
     
     set_bitcoin_cli(args.bitcoin_cli)
@@ -73,3 +80,7 @@ if __name__ == "__main__":
             logger.info("sleeping for 5 minutes")
             time.sleep(60 * 5)
             first_block = curr_height + 1
+
+
+if __name__ == "__main__":
+    main()
