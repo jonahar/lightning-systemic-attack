6c716358814e0be0af78bdf54583846b95f1c496
Jona Harris <jonahar@gmail.com>
2020-03-04 12:17:37 +0200

    don't just skip displaying num_blocks that were not chosen, but don't load them at all

--- py/feerates/graphs/estimated_feerates.py
+++ py/feerates/graphs/estimated_feerates.py
@@ -35,8 +35,11 @@ def parse_estimation_files() -> Dict[int, List[PlotData]]:
         match = estimation_sample_file_regex.match(entry)
         if not match:
             continue  # not an estimation file
-        blocks: int = int(match.group(1))
+        num_blocks: int = int(match.group(1))
         mode: str = match.group(2)
+        if num_blocks_to_include is not None and num_blocks not in num_blocks_to_include:
+            continue
+
         timestamps = []
         feerates = []
         with open(os.path.join(FEE_ESTIMATIONS_DIR, entry)) as f:
@@ -52,9 +55,9 @@ def parse_estimation_files() -> Dict[int, List[PlotData]]:
                         feerates.append(feerate)
                 except ValueError:
                     logger.error(f"ignoring line in file `{entry}` with unexpected format: `{line}`")
-        
-        data[blocks].append(
-            PlotData(timestamps=timestamps, feerates=feerates, label=f"estimatesmartfee(n={blocks},mode={mode})")
+
+        data[num_blocks].append(
+            PlotData(timestamps=timestamps, feerates=feerates, label=f"estimatesmartfee(n={num_blocks},mode={mode})")
         )
     
     return data
@@ -74,19 +77,18 @@ def main():
     
     p_values = [0.2, 0.5, 0.8]
     for num_blocks, plot_data_list in data.items():
-        if num_blocks_to_include is None or num_blocks in num_blocks_to_include:
-            for plot_data in plot_data_list:
-                fig = plot_figure(title=f"estimated feerates", plot_data_list=[plot_data])
-                plt.figure(fig.number)
-                for p in p_values:
-                    generalized_median = get_top_p_minimal_feerate(plot_data.feerates, p=p)
-                    plt.hlines(
-                        y=generalized_median,
-                        xmin=plot_data.timestamps[0],
-                        xmax=plot_data.timestamps[-1],
-                        label=f"top {p} estimates ({generalized_median})",
-                    )
-                plt.legend(loc="best")
+        for plot_data in plot_data_list:
+            fig = plot_figure(title=f"estimated feerates", plot_data_list=[plot_data])
+            plt.figure(fig.number)
+            for p in p_values:
+                generalized_median = get_top_p_minimal_feerate(plot_data.feerates, p=p)
+                plt.hlines(
+                    y=generalized_median,
+                    xmin=plot_data.timestamps[0],
+                    xmax=plot_data.timestamps[-1],
+                    label=f"top {p} estimates ({generalized_median})",
+                )
+            plt.legend(loc="best")
     
     plt.show()
 
