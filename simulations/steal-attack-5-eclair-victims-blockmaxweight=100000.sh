7e9d04331ea62163a1479430439ceab1e73943cd
Jona Harris <jonahar@gmail.com>
2020-03-12 10:47:35 +0200

    add simulation files

---
+++ simulations/steal-attack-5-eclair-victims-blockmaxweight=100000.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+SCRIPT_NAME=$(basename -s ".sh" $0)
+DATA_DIR="$LN/simulations/$SCRIPT_NAME"
+OUTPUT_FILE="$LN/simulations/$SCRIPT_NAME.out"
+TOPOLOGY="$LN/topologies/topology-5-eclair-victims.json"
+SIMULATION=2
+COMMANDS_FILE=$LN/generated_commands_$SIMULATION
+cd $LN/py
+python3 -m commands_generator.commands_generator \
+    --topology "$TOPOLOGY" \
+    --establish-channels \
+    --make-payments 1 3 5000 11000000 \
+    --steal-attack 1 3 200 \
+    --dump-data "$DATA_DIR" \
+    --block-time 150 \
+    --bitcoin-blockmaxweight 100000 \
+    --simulation-number $SIMULATION \
+    --outfile $COMMANDS_FILE
+
+rm -rf /tmp/lightning-simulations/$SIMULATION
+bash $COMMANDS_FILE 2>&1 | tee "$OUTPUT_FILE"
